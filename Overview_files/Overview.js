72e8e7efbd90a549df3e133483225130a99fef28
Paul Tavenner <paultavenner@Pauls-MBP.home>
2018-08-12 18:45:09 -0700

    Moved GoDaddy old website to repository

---
+++ Overview_files/Overview.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.3 local-build-20080522
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Overview_files/OverviewMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
